4c56e20f75a4e713b0cf910ccd7b7c7f2c2753e6
aaleshin <arina.aleshina@intel.com>
2018-10-10 21:42:58 +0300

    changed files in tomita

--- ling_systems/tomita/practice_3/bornin.cxx
+++ ling_systems/tomita/practice_3/bornin.cxx
@@ -1,6 +1,6 @@
 #encoding "utf-8"
 Born -> Verb<kwtype=born>;
-City -> Noun<kwtype=city>;
+City -> Noun<gram="geo"> | Ajd<gnc-agr[1]> Noun<gram="geo", gnc-agr[1] | "село" Noun;
 Person -> AnyWord<gram="persn"> AnyWord<gram="famn"> | AnyWord<gram="persn">;
 Date -> Word<wff=/\d{1,2}/> Noun<kwtype=month> Word<wfl=/\d{4}/>;
 S -> Person interp(BornFact.Person) Born "в" City interp(BornFact.Place);
